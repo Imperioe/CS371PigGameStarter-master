72c3675c278c062234a74e688cdd58212e19a975
SBEBS <ericimperio@yahoo.com>
2016-10-12 15:18:50 -0700

    Actions

---
+++ app/src/main/java/edu/up/cs301/game/actionMsg/PigHoldAction.java
@@ -0,0 +1,13 @@
+package edu.up.cs301.game.actionMsg;
+
+import edu.up.cs301.game.GamePlayer;
+
+/**
+ * Created by Eric Imperio on 10/12/2016.
+ */
+
+public class PigHoldAction extends GameAction {
+    public PigHoldAction(GamePlayer player) {
+        super(player);
+    }
+}
---
+++ app/src/main/java/edu/up/cs301/game/actionMsg/PigRollAction.java
@@ -0,0 +1,13 @@
+package edu.up.cs301.game.actionMsg;
+
+import edu.up.cs301.game.GamePlayer;
+
+/**
+ * Created by SBEBS on 10/12/2016.
+ */
+
+public class PigRollAction extends GameAction {
+    public PigRollAction(GamePlayer player){
+        super(player);
+    }
+}
